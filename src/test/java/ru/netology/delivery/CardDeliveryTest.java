96d48f406f001632b949651eb6bd59ddea946d8a
Mikhail141 <70147484+Mikhail141@users.noreply.github.com>
2020-12-02 13:20:24 +0300

    test

--- src/test/java/ru/netology/delivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/CardDeliveryTest.java
@@ -1,27 +1,24 @@
 package ru.netology.delivery;
-
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
 
+import  java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
-
-
-
     public class CardDeliveryTest {
+        LocalDate date = LocalDate.now();
 
         @Test
 
         void shouldRequest(){
-            LocalDate date = new LocalDate.now();
             open( "http://localhost:9999");
             $("[data-test-id=city] input").setValue("Хабаровск");
-            $("[data-test-id=date] input").setValue("11.12.2020");
+            $(date.format(DateTimeFormatter.ofPattern("19::20::2020")));
             $("[data-test-id=name] input").setValue("Олегов Олег");
             $("[data-test-id=phone] input").setValue("+79999992221");
             $("[data-test-id=agreement]").click();
