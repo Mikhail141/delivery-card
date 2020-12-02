6deee0f321f3a7d144dd435c69dc605ce26910bc
Mikhail141 <70147484+Mikhail141@users.noreply.github.com>
2020-12-02 19:03:50 +0300

    test

--- src/test/java/ru/netology/delivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/CardDeliveryTest.java
@@ -10,23 +10,23 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
-    public class CardDeliveryTest {
+public class CardDeliveryTest {
+    LocalDate date = LocalDate.now();
+
+
+    @Test
+    void shouldRequest() {
         LocalDate date = LocalDate.now();
+        date = date.plusDays(3);
 
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Казань");
+        $("[data-test-id=date] input").setValue(date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+        $("[data-test-id=name] input").setValue("Олегов Олег");
+        $("[data-test-id=phone] input").setValue("+79999992221");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(withText("Успешно!")).waitUntil(visible, 15000);
 
-        @Test
-        void shouldRequest(){
-            LocalDate date = LocalDate.now();
-            date = date.plusDays(3);
-
-            open( "http://localhost:9999");
-            $("[data-test-id=city] input").setValue("Казань");
-            $("[data-test-id=date] input").setValue(date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-            $("[data-test-id=name] input").setValue("Олегов Олег");
-            $("[data-test-id=phone] input").setValue("+79999992221");
-            $("[data-test-id=agreement]").click();
-            $$("button").find(exactText("Забронировать")).click();
-            $(withText("Успешно!")).waitUntil(visible,15000);
-
-        }
     }
+}
