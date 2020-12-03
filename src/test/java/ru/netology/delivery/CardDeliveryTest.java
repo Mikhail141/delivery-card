039cd5ff81de5e26b394222cf57aca623d9f70fb
Mikhail141 <70147484+Mikhail141@users.noreply.github.com>
2020-12-03 12:01:09 +0300

    test

--- src/test/java/ru/netology/delivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/CardDeliveryTest.java
@@ -11,16 +11,13 @@ import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
-    LocalDate date = LocalDate.now();
-
 
     @Test
     void shouldRequest() {
         LocalDate date = LocalDate.now();
         date = date.plusDays(3);
-
         open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue("Казань");
+        $("[data-test-id=city] input").setValue("Хабаровск");
         $("[data-test-id=date] input").setValue(date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
         $("[data-test-id=name] input").setValue("Олегов Олег");
         $("[data-test-id=phone] input").setValue("+79999992221");
